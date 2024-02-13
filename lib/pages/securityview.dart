ba891a657e1fd527979f04f3ba83cc813677ea1b
asnamv08 <asnamvarghese001@gmail.com>
2024-02-14 00:10:33 +0530

    security

--- lib/pages/securityview.dart
+++ lib/pages/securityview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:security_app/models/securitymodel.dart';
+import 'package:security_app/services/securityservice.dart';
 
 class sview extends StatefulWidget {
   const sview({super.key});
@@ -8,10 +10,48 @@ class sview extends StatefulWidget {
 }
 
 class _sviewState extends State<sview> {
+  Future<List<Posts>>? data;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    data=SecurityApiServce().getPosts();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back),
+        ),
+        body: FutureBuilder(
+            future: data,
+            builder: (context,snapshot){
+              if(snapshot.hasData && snapshot.data!.isNotEmpty){
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder:(value,index){
+                      return Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                                title:  Text("Name: "+snapshot.data![index].name.toString()),
+                                subtitle: Text("Empoloyee Id:"+snapshot.data![index].empid.toString()+"\n"+"Address:"+snapshot.data![index].address.toString()+"\n"+"Phone:"+snapshot.data![index].phoneno.toString()+"\n"+"Email:"+snapshot.data![index].email.toString()+"\n"+"Password:"+snapshot.data![index].password.toString()),
+                                leading:CircleAvatar(child: Text(snapshot.data![index].name.toString()[0]),)
+                            )
+                          ],
+                        ),
+                      );
+
+                    }
+                );
+              }
+              else{
+                return Text("Fetching data");
+              }
+
+            }
+
+        ),
 
       ),
     );
---
+++ lib/services/securityservice.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:http/http.dart'as http;
+import 'package:security_app/models/securitymodel.dart';
+
+class SecurityApiServce {
+  Future<dynamic> senddata(String name,
+      String age,
+      String empid,
+      String address,
+      String phone,
+      String emailid,
+      String password) async {
+    var client = http.Client();
+    var url = Uri.parse("http://localhost:3001/api/security/add");
+    var response = await client.post(url,
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: jsonEncode(<String, String>{
+          "name":name,
+          "empid":empid,
+          "address":address,
+          "phoneno":phone,
+          "email":emailid,
+          "password":password
+        })
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    else {
+      throw Exception("Fail to send data");
+    }
+  }
+  Future<List<Posts>>getPosts()async{
+    var client=http.Client();
+    var apiurl=Uri.parse("http://localhost:3001/api/security/view");
+    var response=await client.get(apiurl);
+    if(response.statusCode==200)
+    {
+      return postsFromJson(response.body);
+    }
+    else
+    {
+      return [];
+    }
+
+  }
+}
